1d5239111ffad2e38f4f0906ed8ab1ac502cd542
seonghj <anis15740@gmail.com>
2017-12-09 17:16:57 +0900

    map
    x

--- map.cpp
+++ map.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "Map.h"
 
-Map::Map() : mapSize(40)
+Map::Map() : mapSize(15)
 {
 	arr = new MapInfo*[mapSize];
 	//mapinfo 주소가르키는 포인터 mapsize만큼 할당
@@ -9,27 +9,25 @@ Map::Map() : mapSize(40)
 	{
 		arr[i] = new MapInfo[mapSize];
 		for (int j = 0; j < mapSize; ++j)
-			arr[i][j].Map_pos = Vector3D{ static_cast<float>(j), static_cast<float>(i), 0.0f };
+			arr[i][j].Map_pos = Vector3D{ static_cast<float>(j * 20), 0.0f, static_cast<float>(i * 20) };
 	}
 	//각배열 가리키는 포인터에 배열 할당
 }
 void Map::MapRender()
 {
-	
+
 	for (int i = 0; i < mapSize; ++i)
 	{
 		for (int j = 0; j < mapSize; ++j)
 		{
-			//cout << arr[i][j].Map_pos.x << " " << arr[i][j].Map_pos.y << " " << arr[i][j].Map_pos.z << endl;
+			/*cout << arr[i][j].Map_pos.x << " " << arr[i][j].Map_pos.y << " " << arr[i][j].Map_pos.z << endl;*/
 			glPushMatrix();
 			{
-				glScalef(2, 2, 2);
-				//glTranslatef(-25, -10, 20);
-				//glRotatef(90, 1.0, 0.0, 0.0);
+				glTranslatef(-150, -60, -150);
 				Vector3D pos = arr[i][j].Map_pos;
-				(i+j) % 2 == 0 ? glColor3f(1.0f, 1.0f, 0.0f) : glColor3f(0.0f, 0.0f, 0.0f);
+				(i + j) % 2 == 0 ? glColor3f(1.0f, 1.0f, 0.0f) : glColor3f(0.0f, 0.0f, 0.0f);
 				glTranslatef(pos.x, pos.y, pos.z);
-				glutSolidCube(1);
+				glutSolidCube(20);
 			}
 			glPopMatrix();
 		}
